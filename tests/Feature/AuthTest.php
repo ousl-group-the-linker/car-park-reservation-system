83c4d8ceca8a0ff1a721fd12f8b7382cca72461b
RasikaChamara <cham.c.max@gmail.com>
2022-12-26 14:52:54 +0530

    Test case added for testing valid registration

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,36 +4,46 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-
+use App\Models\User;
+use App\Models\SriLankaCity;
+use Illuminate\Support\Str;
 class AuthTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testisLoginWorkingForAValidUserTest()
+    public function testValidUserRegistration()
     {
-        $response = $this->post(route("auth.login"), [
-            "email" => "sdsdsd@gmail.com",
-            "password" => "Sdsdsdsd"
+        $email = "haldenNick@gmail.com";
+        $password = Str::random();
+
+        $response = $this->post(route("auth.register"), [
+            "email" => $email,
+            "first_name" => "Nick",
+            "last_name" => "Halden",
+            "address_line_1" => "2276",
+            "address_line_2" => "downing street",
+            "address_line_3" => "",
+            "address_city" => SriLankaCity::all()->random()->id,
+            "contact_number" => "0774569875",
+            "password" => $password,
+            "password_confirmation" => $password,
+            "i_aggree" => "yes"
         ]);
 
-        $response->assertStatus(302);
+        $response->assertStatus(302); //it should redirect
+
+        //show all errors if there is any
+        if(session('errors') != null){ 
+            var_dump(session('errors'));
+        }
+
+        $this->assertNull(session('errors')); //test that there should't be any errors
+
+        $this->assertTrue(User::where("email",$email)->count()>0); //inserted user should be in the database
     }
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testIsLogoutWorking()
-    {
-        $response = $this->post(route("auth.login"), [
-            "email" => "sdsdsd@gmail.com",
-            "password" => "Sdsdsdsd"
-        ]);
-
-        $response->assertStatus(302);
-    }
 }
