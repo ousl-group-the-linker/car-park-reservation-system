f27ec77f743aa9dc0eae95a4db6c8aa02b768248
Anver Dole <mohomadanver@gmail.com>
2022-12-27 13:39:21 +0530

    Unit test added for branch management

---
+++ tests/Unit/BranchManagement/BranchTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\BranchManagement;
+
+use PHPUnit\Framework\TestCase;
+
+class BranchTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
