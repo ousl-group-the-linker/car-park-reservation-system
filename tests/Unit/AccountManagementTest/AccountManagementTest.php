e48f45e2635c996516e4a76141bec9e746321894
RasikaChamara <cham.c.max@gmail.com>
2022-12-30 18:08:16 +0530

    Account Management Tested

--- tests/Unit/AccountManagementTest/AccountManagementTest.php
+++ tests/Unit/AccountManagementTest/AccountManagementTest.php
@@ -1,8 +1,15 @@
 <?php
 
-namespace Tests\Unit\BranchManagement;
+namespace Tests\Unit\AccountManagementTest;
+use App\Models\User;
+use App\Models\SriLankaCity;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Illuminate\Support\Str;
 
-use PHPUnit\Framework\TestCase;
+//use PHPUnit\Framework\TestCase;
 
 class AccountManagementTest extends TestCase
 {
@@ -13,7 +20,27 @@ class AccountManagementTest extends TestCase
      */
     public function testIsProfileUpdatePageLoads()
     {
-        $this->assertTrue(true);
+        $password = "1234567890";
+        $email =  time() . "sae@mail.com";
+
+        $user = User::create([
+            'first_name' => "jhon",
+            'last_name' => "Doe",
+            'email' => $email,
+            'address_line_1' => "250",
+            'address_line_2' => "hill side",
+            'address_line_3' => "downtown",
+            'address_city_id' => SriLankaCity::all()->random()->id,
+            'contact_number' => "0758763147",
+            'password' => Hash::make($password),
+            'role' => User::$ROLE_USER
+        ]);
+
+        // login new user
+        Auth::login($user);
+        $response = $this->get(route("account-management"));
+        $response->assertStatus(200);
+        
     }
 
      /**
@@ -23,6 +50,21 @@ class AccountManagementTest extends TestCase
      */
     public function testIsProfileUpdateSavingCorrectly()
     {
+        $email = "caffery@gmail.com";
+        $password =Str::random();
+
+        $response = $this->post(route("account-management.update"), [
+            "email" => $email,
+            "first_name" => "Neal",
+            "last_name" => "Caffery",
+            "address_line_1" => "110",
+            "address_line_2" => "nyork",
+            "address_line_3" => "",
+            "address_city" => SriLankaCity::all()->random()->id,
+            "contact_number" => "0777536987",
+        ]);
+        $response = $this->post(route("account-management.update"));
+        $response->assertStatus(302);
         $this->assertTrue(true);
     }
      /**
@@ -32,6 +74,22 @@ class AccountManagementTest extends TestCase
      */
     public function testIsPasswordUpdatesCorrectly()
     {
-        $this->assertTrue(true);
+        $new_password =Str::random();
+        $password=Str::random();
+        $response = $this->post(route("auth.register"), [
+            "current_password" => $password,
+            "new_password" => $new_password,
+            "confirm_password"=>$new_password,
+        ]);
+
+
+        if ($new_password==$password) {
+            $response = $this->get(route("account-management.change-password"));
+            $response->assertStatus(302);
+        } else {
+            $response = $this->post(route("account-management"));
+            $response->assertStatus(302);
+        }
+
     }
 }
