74a311d515b8026c2bef3be889fe1e5d98794e5d
RasikaChamara <cham.c.max@gmail.com>
2022-12-30 18:40:39 +0530

    Account Management Test Updated (no password)

--- tests/Unit/AccountManagementTest/AccountManagementTest.php
+++ tests/Unit/AccountManagementTest/AccountManagementTest.php
@@ -18,6 +18,7 @@ class AccountManagementTest extends TestCase
      *
      * @return void
      */
+    use RefreshDatabase;
     public function testIsProfileUpdatePageLoads()
     {
         $password = "1234567890";
@@ -50,8 +51,27 @@ class AccountManagementTest extends TestCase
      */
     public function testIsProfileUpdateSavingCorrectly()
     {
-        $email = "caffery@gmail.com";
-        $password =Str::random();
+        $email =  time() . "se@mail.com";
+        $password="1234567890";
+
+        $user = User::create([
+            'first_name' => "jhon",
+            'last_name' => "Doe",
+            'email' => $email,
+            'address_line_1' => "250",
+            'address_line_2' => "hill side",
+            'address_line_3' => "downtown",
+            'address_city_id' => SriLankaCity::all()->random()->id,
+            'contact_number' => "0758763147",
+            'password' => Hash::make($password),
+            'role' => User::$ROLE_USER
+        ]);
+
+        // login new user
+        Auth::login($user);
+
+        $email = "cafferry@gmail.com";
+        
 
         $response = $this->post(route("account-management.update"), [
             "email" => $email,
@@ -65,7 +85,44 @@ class AccountManagementTest extends TestCase
         ]);
         $response = $this->post(route("account-management.update"));
         $response->assertStatus(302);
-        $this->assertTrue(true);
+        
+    }
+
+    public function testIsProfileUpdateSavingWrong(){
+        $email =  time() . "kae@mail.com";
+        $password="1234567890";
+
+        $user = User::create([
+            'first_name' => "jhon",
+            'last_name' => "Doe",
+            'email' => $email,
+            'address_line_1' => "250",
+            'address_line_2' => "hill side",
+            'address_line_3' => "downtown",
+            'address_city_id' => SriLankaCity::all()->random()->id,
+            'contact_number' => "0758763147",
+            'password' => Hash::make($password),
+            'role' => User::$ROLE_USER
+        ]);
+
+        // login new user
+        Auth::login($user);
+        
+
+        $response = $this->post(route("account-management.update"), [
+            
+            "first_name" => "Neal",
+            "last_name" => "Caffery",
+            "address_line_1" => "110",
+            "address_line_2" => "nyork",
+            "address_line_3" => "",
+            "address_city" => -1000,
+            "contact_number" => "0777536987",
+        ]);
+        $response = $this->post(route("account-management.update"));
+        $response->assertStatus(302);
+        
+
     }
      /**
      * Check whether the account password is correctly updates
@@ -75,8 +132,26 @@ class AccountManagementTest extends TestCase
     public function testIsPasswordUpdatesCorrectly()
     {
         $new_password =Str::random();
-        $password=Str::random();
-        $response = $this->post(route("auth.register"), [
+        $password="1234567890";
+        $email =  time() . "sae@mail.com";
+
+        $user = User::create([
+            'first_name' => "jhon",
+            'last_name' => "Doe",
+            'email' => $email,
+            'address_line_1' => "250",
+            'address_line_2' => "hill side",
+            'address_line_3' => "downtown",
+            'address_city_id' => SriLankaCity::all()->random()->id,
+            'contact_number' => "0758763147",
+            'password' => Hash::make($password),
+            'role' => User::$ROLE_USER
+        ]);
+
+        // login new user
+        Auth::login($user);
+        
+        $response = $this->post(route("account-management.change-password"), [
             "current_password" => $password,
             "new_password" => $new_password,
             "confirm_password"=>$new_password,
