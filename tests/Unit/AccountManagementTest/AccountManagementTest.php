efa0eb7c9380ec50cfc5d58ec78e7fbec415d6a2
Anver Dole <mohomadanver@gmail.com>
2022-12-30 17:01:19 +0530

    unit test template added for account management

---
+++ tests/Unit/AccountManagementTest/AccountManagementTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\BranchManagement;
+
+use PHPUnit\Framework\TestCase;
+
+class AccountManagementTest extends TestCase
+{
+    /**
+     * Check whether the profule infomatuin is loading
+     *
+     * @return void
+     */
+    public function testIsProfileUpdatePageLoads()
+    {
+        $this->assertTrue(true);
+    }
+
+     /**
+     * Check whether the profule infomatuin is updating
+     *
+     * @return void
+     */
+    public function testIsProfileUpdateSavingCorrectly()
+    {
+        $this->assertTrue(true);
+    }
+     /**
+     * Check whether the account password is correctly updates
+     *
+     * @return void
+     */
+    public function testIsPasswordUpdatesCorrectly()
+    {
+        $this->assertTrue(true);
+    }
+}
