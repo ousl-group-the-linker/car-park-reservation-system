86fe1b2c20b7416d836fc60908a3df411b73c041
Anver Dole <mohomadanver@gmail.com>
2023-01-08 04:04:56 +0530

    Seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->role(User::$ROLE_SUPER_ADMIN)->create();
-        User::factory(10)->role(User::$ROLE_MANAGER)->create();
-        User::factory(10)->role(User::$ROLE_COUNTER)->create();
-        User::factory(10)->role(User::$ROLE_USER)->create();
+        User::factory(1)->role(User::$ROLE_SUPER_ADMIN)->create();
+        User::factory(1)->role(User::$ROLE_MANAGER)->create();
+        User::factory(1)->role(User::$ROLE_COUNTER)->create();
+        User::factory(1)->role(User::$ROLE_USER)->create();
 
-        Branch::factory(10)->create();
+        Branch::factory(1)->create();
 
         Booking::factory(10)->status(Booking::STATUS_PENDING)->create();
         Booking::factory(10)->status(Booking::STATUS_CANCELLED)->create();
